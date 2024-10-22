09935d3d90044db3f33ca8e118a4cff6041acd21
wilsonnwafor <nwaforobumneme12@gmail.com>
2024-10-22 02:36:17 +0100

    imports resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Landing from "./components/landing page/Landing";
 import Login from "./components/auth/Login";
-import Reg from "./components/auth/reg";
+// import Reg from "./components/auth/reg";
 import { Route, Routes } from "react-router-dom";
 import Deposit from "./components/Deposit/Deposit";
 import Withdraw from "./components/withdraw/withdraw";
@@ -9,6 +9,7 @@ import Approved_deposit from "./components/approval/approved_deposit";
 import Home from "./components/home/Home";
 import Asset from "./components/asset-comp/Asset";
 import Approved_withraw from "./components/approval/Approved_withraw";
+import Reg from "./components/auth/reg";
 
 
 
@@ -23,13 +24,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/Reg" element={<Reg />} />
+        {/* <Route path="/Reg" element={<Reg />} /> */}
         <Route path="/Home" element={<Home />} />
         <Route path="/Asset" element={<Asset />} />
         <Route path="/Deposit" element={<Deposit />} />
         <Route path="/Withdraw" element={<Withdraw />} />
         <Route path="/Approved_deposit" element={<Approved_deposit />} />
         <Route path="/Approved_withraw" element={<Approved_withraw />} />
+        <Route path="/Reg" element={<Reg />} />
+
       </Routes>
 
       {/* <Reg/> */}
